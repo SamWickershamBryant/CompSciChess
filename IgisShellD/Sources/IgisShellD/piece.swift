39f9fd5343706b030898d754f1cab50e567e154c
247122 <yugi.sosarocha@student.allenisd.org>
2019-09-05 15:11:49 -0500

    Piece class added

---
+++ IgisShellD/Sources/IgisShellD/piece.swift
@@ -0,0 +1,22 @@
+class Piece {
+
+    var type: String
+    var posX : Int
+    var posY : Int
+    let color : String
+    
+    init(type:String, posX:Int, posY:Int, color:String) {
+        self.type = type
+        self.posX = posX
+        self.posY = posY
+        self.color = color        
+    }
+
+
+
+
+
+
+    
+
+}
