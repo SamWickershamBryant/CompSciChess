36f557f52a657b5791444c321ec973976939203e
247122 <yugi.sosarocha@student.allenisd.org>
2019-09-13 15:30:25 -0500

    piece

--- IgisShellD/Sources/IgisShellD/piece.swift
+++ IgisShellD/Sources/IgisShellD/piece.swift
@@ -1,17 +1,24 @@
 import Igis
 
-class Piece {
+class Piece : CustomStringConvertible {
 
     let type : String
     let color : String // "b" or "w"
+    var position : String // Placeholder 
     var hasMoved : Bool
     
-    init(type:String, color:String) {
+    init(type:String, color:String, position: String, hasMoved: Bool) {
         self.type = type
         self.color = color
+        self.position = position
+        self.hasMoved = hasMoved
     }
 
     func id() -> String {
-        return "Color: \(self.color), Type: \(self.type)" 
+        return "Color: \(self.color), Type: \(self.type), \(position)" 
+    }
+    
+    var description : String {
+        return "Type:\(type), Color:\(color), Position:\(position), hasMoved:\(hasMoved)"
     }
 }
