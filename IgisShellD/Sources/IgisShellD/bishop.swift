cce046570611039122ca46905f4120ebaf23b9ee
nickaz246 <nicholas.azar@student.allenisd.org>
2019-09-25 14:41:11 -0500

    bishop?

--- IgisShellD/Sources/IgisShellD/bishop.swift
+++ IgisShellD/Sources/IgisShellD/bishop.swift
@@ -7,11 +7,13 @@ class Bishop : Piece {
     }
  
     override func moveList() -> [Point] {
-       
+        let unfilteredLegalMoves = [Point(x:self.position.x+1,y:self.position.y+1),Point(x:self.position.x+1,y:self.position.y-1),
+                                    Point(x:self.position.x-1,y:self.position.y-1),Point(x:self.position.x-1,y:self.position.y+1)]
+        return unfilteredLegalMoves.filter({Board.inBounds($0)})
     }
 
     override func legalMoves() -> [Point] {
-
+        return moveList()
     }
 
 
