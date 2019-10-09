9b3aae44f6b026c09e4b05e51c21f882ab27e325
BryantSam <samwbryant@gmail.com>
2019-10-09 15:31:07 -0500

    started pawn moves

--- IgisShellD/Sources/IgisShellD/pawn.swift
+++ IgisShellD/Sources/IgisShellD/pawn.swift
@@ -1,5 +1,5 @@
 import Igis
-
+import Foundation
 class Pawn : Piece {
 
 
@@ -12,6 +12,32 @@ class Pawn : Piece {
 
     
     override func moveList(boardstate:[[Piece?]]) -> [Point] {
+        var attackMoves : [Point] = []
+        var passiveMoves : [Point] = []
+        if self.color == "w" {
+        let forward1 = Point(x:self.position.x, y:self.position.y - 1)
+        let forward2 = Point(x:self.position.x, y:self.position.y - 2)
+        let upLeft = Point(x:self.position.x - 1, y:self.position.y - 1)
+        let upRight = Point(x:self.position.x + 1, y:self.position.y - 1)
+        if Board.pieceAt(forward1, boardstate:boardstate) == nil {
+            passiveMoves.append(forward1)
+            if Board.pieceAt(forward2, boardstate:boardstate) == nil && self.hasMoved == false {
+                passiveMoves.append(forward2)
+            }
+        }
+        let upLeftPiece = Board.pieceAt(upLeft, boardstate:boardstate)
+        let upRightPiece = Board.pieceAt(upRight, boardstate:boardstate)
+        if upLeftPiece != nil {
+            if upLeftPiece!.color == "b" {
+                attackMoves.append(upLeft)
+            }
+        }
+        } else if self.color == "b" {
+            let forward1 = Point(x:self.position.x, y:self.position.y + 1)
+            let forward2 = Point(x:self.position.x, y:self.position.y + 2)
+            let upLeft = Point(x:self.position.x - 1, y:self.position.y + 1)
+            let upRight = Point(x:self.position.x + 1, y:self.position.y + 1)
+        }
        
         
     }
