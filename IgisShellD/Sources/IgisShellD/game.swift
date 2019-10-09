e5351ae1d5f9b1fb45a0e6e66aea884bdb66d768
ColinNeary <colin-neary@student.allenisd.org>
2019-10-09 15:08:11 -0500

    started on gameSize

--- IgisShellD/Sources/IgisShellD/game.swift
+++ IgisShellD/Sources/IgisShellD/game.swift
@@ -1,5 +1,30 @@
+import Igis
+import Foundation
+
 class Game {
 
-    init() {
+    let board : Board
+    let topMargin : Rect
+    let bottomMargin : Rect
+    let sideBar : Rect
+
+    init(gameSize:Rect) {
+        let gameHeight = gameSize.size.height
+        let gameWidth = gameSize.size.width
+        
+        topMargin = Rect(topLeft:Point(x:gameSize.topLeft.x, y:gameSize.topLeft.y),
+                         size: Size(width: (gameWidth * 65) / 100, height: gameHeight / 10))
+        let boardTopLeft = Point(x:gameSize.topLeft.x,
+                                 y:gameSize.topLeft.y + topMargin.size.height)
+                
+        let boardSize = Size(width:topMargin.size.width, height:topMargin.size.width)
+        bottomMargin = Rect(topLeft:Point(x:gameSize.topLeft.x, y:gameSize.topLeft.y + topMargin.size.height + boardSize.height),
+                            size:Size(width:topMargin.size.width, height:gameSize.size.height - boardSize.height - topMargin.size.height))
+        
+        self.board = Board(topLeft:, size:,  boardstate:, whosMove:, outLineColor:, inLinecolor:, squareColor:, lineWidth:)
+    }
+
+    func onClick(point:Point) -> Point {
+        if point.x < board
     }
 }
